83ee9d2019dfc22b98c8797a9a55b63f337181b9
Martin List <mlist@eos-uptrade.de>
2023-08-10 15:58:29 +0200

    Adds SampleApp target for cleaner description

--- SampleProject/SampleApp/SampleApp.swift
+++ SampleProject/SampleApp/SampleApp.swift
@@ -1,6 +1,6 @@
 //
-//  SampleProjectApp.swift
-//  SampleProject
+//  SampleApp.swift
+//  SampleApp
 //
 //  Created by Martin List on 10.08.23.
 //
@@ -9,7 +9,7 @@ import SwiftUI
 import SampleFramework
 
 @main
-struct SampleProjectApp: App {
+struct SampleAppApp: App {
     var body: some Scene {
         WindowGroup {
             SampleView()
